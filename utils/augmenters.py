9f18f54109e424422ed537fb87629ef5fbd323c8
xinyuliu-jeffrey <jeffreylxy@outlook.com>
2020-05-06 10:11:17 +0800

    support hflip & vflip
    Enlarge the training set to 5k.(1250ori 1250rot 1250hflip 1250vflip)

--- utils/augmenters.py
+++ utils/augmenters.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import cv2
 import SimpleITK as sitk
-import torch
+import torch, torchvision
 import scipy.ndimage
 import random
 from PIL import Image
@@ -16,9 +16,20 @@ def select_data_for_augmentation(img_path, count):
             for filename in fileList:
                 if "mask" not in filename.lower():
                   img, lbl = Image.open(img_path+filename), Image.open(img_path+filename[:-4]+'_mask.png')
+                  # Do rotate
                   img_rot, lbl_rot = rotate(img, lbl)
                   cv2.imwrite('./imgs/'+str(count)+'.png',np.array(img_rot))
                   cv2.imwrite('./imgs/'+str(count)+'_mask.png',np.array(lbl_rot))
+                  count+=1
+                  # Do horizontal flip
+                  img_hf, lbl_hf = horizontal_flip(img, lbl)
+                  cv2.imwrite('./imgs/'+str(count)+'.png',np.array(img_hf))
+                  cv2.imwrite('./imgs/'+str(count)+'_mask.png',np.array(lbl_hf))
+                  count+=1
+                  # Do vertical flip
+                  img_vf, lbl_vf = horizontal_flip(img, lbl)
+                  cv2.imwrite('./imgs/'+str(count)+'.png',np.array(img_vf))
+                  cv2.imwrite('./imgs/'+str(count)+'_mask.png',np.array(lbl_vf))
                   count+=1
     return count
                 
@@ -26,6 +37,16 @@ def rotate(img, lbl, theta = None):
     # Rotate volume by a minor angle (+/- 10 degrees: determined by investigation of dataset variability)
     if theta is None:
         theta = random.randint(-10, 10)
-    img_new = scipy.ndimage.interpolation.rotate(img, theta, reshape = False)
-    lbl_new = scipy.ndimage.interpolation.rotate(lbl, theta, reshape = False)
-    return img_new, lbl_new
+    img_rot = scipy.ndimage.interpolation.rotate(img, theta, reshape = False)
+    lbl_rot = scipy.ndimage.interpolation.rotate(lbl, theta, reshape = False)
+    return img_rot, lbl_rot
+
+def horizontal_flip(img, lbl):
+    img_hf = torchvision.transforms.functional.hflip(img)
+    lbl_hf = torchvision.transforms.functional.hflip(lbl)
+    return img_hf, lbl_hf
+
+def vertical_flip(img, lbl):
+    img_vf = torchvision.transforms.functional.vflip(img)
+    lbl_vf = torchvision.transforms.functional.vflip(lbl)
+    return img_vf, lbl_vf
